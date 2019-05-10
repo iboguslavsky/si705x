555b95206d8a87553325d1a62d4f5693e956ccda
Igor Boguslavsky <iboguslavsky@gmail.com>
2019-05-10 11:46:42 -0400

    Move constants into class vars not to pollute theglobal  namespace

--- Si705x.py
+++ Si705x.py
@@ -3,39 +3,38 @@ from smbus2 import SMBusWrapper, i2c_msg
 import time
 
 
-# I2C Address of the device
-SI7050_DEFAULT_ADDRESS			= 0x40
-
-# SI7050 Command Set
-SI7050_MEASTEMP_HOLD_CMD		= [0xE3]
-SI7050_MEASTEMP_NOHOLD_CMD		= [0xF3]
-SI7050_RESET_CMD			= [0xFE]
-SI7050_WRITE_USERREG_CMD		= [0xE6]
-SI7050_READ_USERREG_CMD			= [0xE7]
-SI7050_READ_FW_REV			= [0x84, 0xB8]
-SI7050_READ_ID_BYTE1			= [0xFA, 0x0F]
-SI7050_READ_ID_BYTE2			= [0xFC, 0xC9]
-
 class Si705x():
+
+	# I2C Address of the device
+	SI7050_DEFAULT_ADDRESS			= 0x40
+	# SI7050 Command Set
+	SI7050_MEASTEMP_HOLD_CMD		= [0xE3]
+	SI7050_MEASTEMP_NOHOLD_CMD		= [0xF3]
+	SI7050_RESET_CMD			= [0xFE]
+	SI7050_WRITE_USERREG_CMD		= [0xE6]
+	SI7050_READ_USERREG_CMD			= [0xE7]
+	SI7050_READ_FW_REV			= [0x84, 0xB8]
+	SI7050_READ_ID_BYTE1			= [0xFA, 0x0F]
+	SI7050_READ_ID_BYTE2			= [0xFC, 0xC9]
 
 	def __init__ (self, bus):
 		self.bus = bus
 
 	def _send_cmd (self, cmd, num_bytes = 0):
-		w = i2c_msg.write(SI7050_DEFAULT_ADDRESS, cmd)
+		w = i2c_msg.write(self.SI7050_DEFAULT_ADDRESS, cmd)
 		if num_bytes:
-			r = i2c_msg.read(SI7050_DEFAULT_ADDRESS, num_bytes)
+			r = i2c_msg.read(self.SI7050_DEFAULT_ADDRESS, num_bytes)
 			self.bus.i2c_rdwr(w,r)
 			return list(r)
 		else:
 			self.bus.i2c_rdwr(w)
 
 	def reset(self):
-		self._send_cmd(SI7050_RESET_CMD)
+		self._send_cmd(self.SI7050_RESET_CMD)
 		time.sleep(.01)	# the chip needs a little time to come out of reset
 	
 	def getTemp(self):
-		(msb, lsb, crc) = self._send_cmd(SI7050_MEASTEMP_HOLD_CMD, 3)
+		(msb, lsb, crc) = self._send_cmd(self.SI7050_MEASTEMP_HOLD_CMD, 3)
 		crc_init = Si705x.crc8_dallas_maxim(0, msb)
 		assert crc == Si705x.crc8_dallas_maxim(crc_init, lsb), "getTemp(): Bad CRC"
 		cTemp = ((msb * 256 + lsb) * 175.72 / 65536.0) - 46.85
@@ -43,7 +42,7 @@ class Si705x():
 		return cTemp, fTemp
 		
 	def readUserRegister(self):
-		return self._send_cmd(SI7050_READ_USERREG_CMD, 1)[0]
+		return self._send_cmd(self.SI7050_READ_USERREG_CMD, 1)[0]
 
 	def writeUserRegister(self, res1, res0):
 		# Following read-modify-write approach as per datasheet
@@ -57,14 +56,14 @@ class Si705x():
 			current |= res0
 		else:
 			current &= ~res0	
-		setting = SI7050_WRITE_USERREG_CMD
+		setting = self.SI7050_WRITE_USERREG_CMD
 		setting.append(current)
-		w = i2c_msg.write(SI7050_DEFAULT_ADDRESS, setting)
+		w = i2c_msg.write(self.SI7050_DEFAULT_ADDRESS, setting)
 		self.bus.i2c_rdwr(w)
 
 	def getSerialNumber(self):
 		# Left nibble of SN
-		first_access = self._send_cmd(SI7050_READ_ID_BYTE1, 8)
+		first_access = self._send_cmd(self.SI7050_READ_ID_BYTE1, 8)
 		# Break down the data into (data, crc) tuples
 		print(first_access)
 		chunks = (first_access[x:x+2] for x in range(0, len(first_access), 2))
@@ -76,7 +75,7 @@ class Si705x():
 			crc_init = crc
 
 		# Right nibble of SN
-		second_access = self._send_cmd(SI7050_READ_ID_BYTE2, 6)
+		second_access = self._send_cmd(self.SI7050_READ_ID_BYTE2, 6)
 		chunks = (second_access[x:x+3] for x in range(0, len(second_access), 3))
 		crc_init = 0
 		snb = []
@@ -89,7 +88,7 @@ class Si705x():
 		return sna + snb
 
 	def getFirmwareRevision(self):
-		return self._send_cmd(SI7050_READ_FW_REV, 1)[0]
+		return self._send_cmd(self.SI7050_READ_FW_REV, 1)[0]
 
 	@staticmethod
 	def crc8_dallas_maxim(crc, a):
